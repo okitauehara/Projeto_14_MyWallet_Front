5abddd0084e15c9cd678f7702e667513134e34b6
okitauehara <marcosokita@live.com>
2021-10-19 15:46:20 -0300

    Layout base de expenses finalizado

--- src/components/pages/Expenses.js
+++ src/components/pages/Expenses.js
@@ -1,5 +1,48 @@
+import Input from "../shared/Input";
+import Button from "../shared/Button";
+import Form from "../shared/Form";
+import { useState } from "react";
+import PageTitle from "../shared/PageTitle";
+
 export default function Expenses() {
+
+    const [data, setData] = useState({ value: '', description: '' });
+
+    const handleChange = event => {
+        const { name, value } = event.target;
+        setData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    function submitExpenses(event) {
+        event.preventDefault();
+        // Conectar com back-end
+    }
+
     return (
-        <h1>Expenses</h1>
+        <>
+            <PageTitle>Nova Saída</PageTitle>
+            <Form onSubmit={submitExpenses}>
+                <Input
+                    placeholder='Valor'
+                    type='text'
+                    name='value'
+                    required
+                    value={data.value}
+                    onChange={handleChange}
+                />
+                <Input
+                    placeholder='Descrição'
+                    type='text'
+                    name='description'
+                    required
+                    value={data.description}
+                    onChange={handleChange}
+                />
+                <Button>Salvar saída</Button>
+            </Form>
+        </>
     );
 }
