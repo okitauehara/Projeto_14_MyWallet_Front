3e219539c7fb071af763e80c3b7d4eca220ff2bf
okitauehara <marcosokita@live.com>
2021-11-23 20:26:58 -0300

    Fixing some minor indentations

--- src/App.js
+++ src/App.js
@@ -1,48 +1,49 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter, Switch, Route, useHistory,
+} from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import Login from './components/pages/Login';
 import SignUp from './components/pages/SignUp';
 import Home from './components/pages/Home';
 import Earnings from './components/pages/Earnings';
 import Expenses from './components/pages/Expenses';
-import { useEffect, useState } from 'react';
 import UserContext from './contexts/UserContext';
-import { useHistory } from 'react-router';
 
 export default function App() {
-	
-	const [user, setUser] = useState('');
-	const history = useHistory();
+  const [user, setUser] = useState('');
+  const history = useHistory();
 
-	useEffect(() => {
-		setUser(JSON.parse(localStorage.getItem('@user')));
-		if (user === undefined) {
-			history.push('/')
-		}
-		// eslint-disable-next-line
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem('@user')));
+    if (user === undefined) {
+      history.push('/');
+    }
+    // eslint-disable-next-line
 	}, []);
 
-	return (
-		<UserContext.Provider value={{ user }}>
-			<BrowserRouter>
-				<Switch>
-					<Route path='/' exact>
-						<Login setUser={setUser}/>
-					</Route>
-					<Route path='/sign-up' exact>
-						<SignUp />
-					</Route>
-					<Route path='/home' exact>
-						<Home setUser={setUser}/>
-					</Route>
-					<Route path='/new-earning' exact>
-						<Earnings />
-					</Route>
-					<Route path='/new-expense' exact>
-						<Expenses />
-					</Route>
-				</Switch>
-			</BrowserRouter>
-		</UserContext.Provider>
-	);
+  return (
+    <UserContext.Provider value={{ user }}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <Login setUser={setUser} />
+          </Route>
+          <Route path="/sign-up" exact>
+            <SignUp />
+          </Route>
+          <Route path="/home" exact>
+            <Home setUser={setUser} />
+          </Route>
+          <Route path="/new-earning" exact>
+            <Earnings />
+          </Route>
+          <Route path="/new-expense" exact>
+            <Expenses />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </UserContext.Provider>
+  );
 }
--- src/services/API.js
+++ src/services/API.js
@@ -1,68 +1,69 @@
 import axios from 'axios';
+
 const URL_BASE = 'https://my-wallet-database.herokuapp.com';
 
 function createHeaders(token) {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }
-    return config;
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return config;
 }
 
 function postSignUp(body) {
-    const promise = axios.post(`${URL_BASE}/sign-up`, body);
-    return promise;
+  const promise = axios.post(`${URL_BASE}/sign-up`, body);
+  return promise;
 }
 
 function postSignIn(body) {
-    const promise = axios.post(`${URL_BASE}/sign-in`, body);
-    return promise;
+  const promise = axios.post(`${URL_BASE}/sign-in`, body);
+  return promise;
 }
 
 function requestSignOut(token) {
-    const config = createHeaders(token);
-    const promise = axios.delete(`${URL_BASE}/sign-out`, config);
-    return promise;
+  const config = createHeaders(token);
+  const promise = axios.delete(`${URL_BASE}/sign-out`, config);
+  return promise;
 }
 
 function getTransactions(token) {
-    const config = createHeaders(token);
-    const promise = axios.get(`${URL_BASE}/transactions`, config);
-    return promise;
+  const config = createHeaders(token);
+  const promise = axios.get(`${URL_BASE}/transactions`, config);
+  return promise;
 }
 
 function postNewEarning(body, token) {
-    const config = createHeaders(token);
-    const promise = axios.post(`${URL_BASE}/transactions`, body, config);
-    return promise;
+  const config = createHeaders(token);
+  const promise = axios.post(`${URL_BASE}/transactions`, body, config);
+  return promise;
 }
 
 function postNewExpense(body, token) {
-    const config = createHeaders(token);
-    const promise = axios.post(`${URL_BASE}/transactions`, body, config);
-    return promise;
+  const config = createHeaders(token);
+  const promise = axios.post(`${URL_BASE}/transactions`, body, config);
+  return promise;
 }
 
 function deleteTransaction(transactionId, token) {
-    const config = createHeaders(token);
-    const promise = axios.delete(`${URL_BASE}/transactions/${transactionId}`, config);
-    return promise;
+  const config = createHeaders(token);
+  const promise = axios.delete(`${URL_BASE}/transactions/${transactionId}`, config);
+  return promise;
 }
 
 function updateTransaction(transactionId, token, body) {
-    const config = createHeaders(token);
-    const promise = axios.put(`${URL_BASE}/transactions/${transactionId}`, body, config);
-    return promise;
+  const config = createHeaders(token);
+  const promise = axios.put(`${URL_BASE}/transactions/${transactionId}`, body, config);
+  return promise;
 }
 
 export {
-    postSignUp,
-    postSignIn,
-    requestSignOut,
-    getTransactions,
-    postNewEarning,
-    postNewExpense,
-    deleteTransaction,
-    updateTransaction
-}
+  postSignUp,
+  postSignIn,
+  requestSignOut,
+  getTransactions,
+  postNewEarning,
+  postNewExpense,
+  deleteTransaction,
+  updateTransaction,
+};
