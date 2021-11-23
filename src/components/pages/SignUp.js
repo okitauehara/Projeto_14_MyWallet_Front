c052190ce650bee5f2398d27b757ed989601ede0
okitauehara <marcosokita@live.com>
2021-11-23 20:03:37 -0300

    Signup refactored by eslint

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,120 +1,116 @@
-import Input from "../shared/Input";
-import Button from "../shared/Button";
-import Redirect from "../shared/Redirect";
-import Form from "../shared/Form";
-import { CenterPage, Logo } from "../shared/LoginSignUp";
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { postSignUp } from "../../services/API";
-import Swal from "sweetalert2";
-import { useHistory } from "react-router";
+import { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import Input from '../shared/Input';
+import Button from '../shared/Button';
+import Redirect from '../shared/Redirect';
+import Form from '../shared/Form';
+import { CenterPage, Logo } from '../shared/LoginSignUp';
+import { postSignUp } from '../../services/API';
 
 export default function SignUp() {
+  const [data, setData] = useState({
+    name: '', email: '', password: '', confirmation: '',
+  });
+  const [isDisabled, setIsDisabled] = useState(false);
+  const history = useHistory();
+  const emailRegex = '[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}';
 
-    const [data, setData] = useState({ name: '', email: '', password: '', confirmation: '' });
-    const [isDisabled, setIsDisabled] = useState(false);
-    const history = useHistory();
-    const emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}";
+  const handleChange = (event) => {
+    setData({ ...data, [event.target.name]: [event.target.value] });
+  };
 
-    const handleChange = event => {
-        const { name, value } = event.target;
-        setData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
+  function submitSignUp(event) {
+    event.preventDefault();
+    setIsDisabled(true);
+    const body = {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      confirmation: data.confirmation,
     };
 
-    function submitSignUp(event) {
-        event.preventDefault();
-        setIsDisabled(true);
-        const body = {
-            name: data.name,
-            email: data.email,
-            password: data.password,
-            confirmation: data.confirmation
+    postSignUp(body)
+      .then(() => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Usuário cadastrado!',
+        });
+        setIsDisabled(false);
+        history.push('/');
+      })
+      .catch((error) => {
+        if (error.response.status === 400) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Verifique se todos os dados inseridos são válidos',
+          });
         }
-        
-        postSignUp(body)
-            .then(() => {
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Usuário cadastrado!',
-                })
-                setIsDisabled(false);
-                history.push('/');
-            })
-            .catch((error) => {
-                if (error.response.status === 400) {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Verifique se todos os dados inseridos são válidos',
-                    })
-                }
-                if (error.response.status === 409) {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'O e-mail inserido já está em uso',
-                    })
-                }
-                setIsDisabled(false);
-            })
-    }
+        if (error.response.status === 409) {
+          Swal.fire({
+            icon: 'error',
+            title: 'O e-mail inserido já está em uso',
+          });
+        }
+        setIsDisabled(false);
+      });
+  }
 
-    return (
-        <CenterPage>
-            <Logo>MyWallet</Logo>
-            <Form onSubmit={submitSignUp} >
-                <Input
-                    placeholder='Nome'
-                    type='text'
-                    name='name'
-                    required
-                    autoFocus
-                    minLength='3'
-                    value={data.name}
-                    onChange={handleChange}
-                    disabled={isDisabled}
-                    validation={true}
-                />
-                <Input
-                    placeholder='E-mail'
-                    type='email'
-                    name='email'
-                    required
-                    pattern={emailRegex}
-                    value={data.email}
-                    onChange={handleChange}
-                    disabled={isDisabled}
-                    validation={true}
-                />
-                <Input
-                    placeholder='Senha'
-                    type='password'
-                    name='password'
-                    required
-                    minLength='8'
-                    value={data.password}
-                    onChange={handleChange}
-                    disabled={isDisabled}
-                    validation={true}
-                />
-                <Input
-                    placeholder='Confirme a senha'
-                    type='password'
-                    name='confirmation'
-                    required
-                    minLength='8'
-                    pattern={data.password}
-                    value={data.confirmation}
-                    onChange={handleChange}
-                    disabled={isDisabled}
-                    validation={true}
-                />
-                <Button disabled={isDisabled}>Cadastrar</Button>
-            </Form>
-            <Link to='/' disabled={isDisabled} style={{ pointerEvents: isDisabled ? 'none' : 'all', textDecoration: 'none' }}>
-                <Redirect>Já tem uma conta? Entre agora!</Redirect>
-            </Link>
-        </CenterPage>
-    );
+  return (
+    <CenterPage>
+      <Logo>MyWallet</Logo>
+      <Form onSubmit={submitSignUp}>
+        <Input
+          placeholder="Nome"
+          type="text"
+          name="name"
+          required
+          autoFocus
+          minLength="3"
+          value={data.name}
+          onChange={handleChange}
+          disabled={isDisabled}
+          validation
+        />
+        <Input
+          placeholder="E-mail"
+          type="email"
+          name="email"
+          required
+          pattern={emailRegex}
+          value={data.email}
+          onChange={handleChange}
+          disabled={isDisabled}
+          validation
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          name="password"
+          required
+          minLength="8"
+          value={data.password}
+          onChange={handleChange}
+          disabled={isDisabled}
+          validation
+        />
+        <Input
+          placeholder="Confirme a senha"
+          type="password"
+          name="confirmation"
+          required
+          minLength="8"
+          pattern={data.password}
+          value={data.confirmation}
+          onChange={handleChange}
+          disabled={isDisabled}
+          validation
+        />
+        <Button disabled={isDisabled}>Cadastrar</Button>
+      </Form>
+      <Link to="/" disabled={isDisabled} style={{ pointerEvents: isDisabled ? 'none' : 'all', textDecoration: 'none' }}>
+        <Redirect>Já tem uma conta? Entre agora!</Redirect>
+      </Link>
+    </CenterPage>
+  );
 }
